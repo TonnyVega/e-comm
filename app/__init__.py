ebd2542b6b420bcfc937d2c58b7878679696afec
TonnyVega <tonycndirangu@gmail.com>
2024-04-25 22:05:39 +0300

    sqlite db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from app.models import db
+
 
 app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecomm-db.db' 
+db.init_app(app)
 
 
 from app import routes, models
